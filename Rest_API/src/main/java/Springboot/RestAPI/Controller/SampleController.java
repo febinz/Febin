424f1d0a6505aaa4923cfe05a3de1fe27f0a979d
USER <USER@DESKTOP-7VBNMO1>
2021-02-04 19:41:19 +0000

    hgyg

--- Rest_API/src/main/java/Springboot/RestAPI/Controller/SampleController.java
+++ Rest_API/src/main/java/Springboot/RestAPI/Controller/SampleController.java
@@ -1,19 +1,25 @@
-package Springboot.RestAPI.Sample;
+package Springboot.RestAPI.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import Springboot.RestAPI.model.Model;
+import Springboot.RestAPI.service.Service;
+
 @RestController
 public class SampleController {
+	
+	@Autowired
+	private Service service;
 	
 	@RequestMapping("/Hello")
 	public String test() {
 		return "Hello";
 		
 	}
-	
 	
 	@RequestMapping(method = RequestMethod.POST, value= "/Hello")
 	public String testPost(@RequestBody Model model) {
